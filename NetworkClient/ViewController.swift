806f857651cb463ff4b0e90ebee61393bbd6e007
bigbaby <handongwang@handongdemacbook-pro.local>
2018-06-15 11:33:44 +0800

    Initial Commit

---
+++ NetworkClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkClient
+//
+//  Created by HanDong Wang on 2018/6/15.
+//  Copyright © 2018年 HanDong Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
